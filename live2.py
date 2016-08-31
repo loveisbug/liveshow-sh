5eabdedf0008afff9a69eb4cb620d8e1066f8f55
angellaugh <972235265@qq.com>
2016-08-31 12:29:50 +0800

    print to file(txt)

--- live2.py
+++ live2.py
@@ -3,10 +3,15 @@
 import urllib
 from urllib.request import urlopen
 import html.parser as h
-
-
 from bs4 import BeautifulSoup
 import sys
+import time
+import io
+
+# 创建以日期命名的txt文件, print 时加入file = file1可重写入该文件
+crFile = time.strftime("%Y%m%d", time.localtime())
+file1 = open("LiveNews" + crFile + ".txt", 'w')
+
 
 def fetchMao():
     urlrequest = urlopen('https://site.douban.com/maosh/widget/events/1441569/?start=0')
@@ -14,9 +19,10 @@ def fetchMao():
     parser = BeautifulSoup(urlrequest, "html.parser")
     elist = parser.find('div', 'events-list-s').findAll('li', 'item')
     for event in elist:
-        print(event.findNext('a').text)
-        print(event.findNext('p').text)
-        print(event.findNext('a')['href'])
+        # with open(file1, 'wt') as f:
+        print(event.findNext('a').text, file = file1)
+        print(event.findNext('p').text, file = file1)
+        print(event.findNext('a')['href'], file = file1)
 
 
 
@@ -25,15 +31,15 @@ def fetchMao():
     parser2 = BeautifulSoup(url2, 'html.parser')
     firstendevent = parser2.find('div', 'events-list-s').findAll('li', 'item close')
     for firstend in firstendevent:
-        print('this is the first closed itme')
-        print(firstend.find('a').text)
-        print(firstend.find('p').text)
-        print(firstend.find('a')['href'])
+        print('this is the first closed itme', file = file1)
+        print(firstend.find('a').text, file = file1)
+        print(firstend.find('p').text, file = file1)
+        print(firstend.find('a')['href'], file = file1)
 
     # 搜索所有翻页并打印出
     alist = parser.find('div', 'paginator').findAll('a')
     for ahref in alist:
-        print('https://site.douban.com/'+ ahref.findNext('a')['href'])
+        print('https://site.douban.com/'+ ahref.findNext('a')['href'], file = file1)
 
 
 def main(argv):
