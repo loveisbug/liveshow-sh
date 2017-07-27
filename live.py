82c3ec5631db48f8662fc06af0b3fecd6634a78c
loveisbug <ericnomail@gmail.com>
2017-07-27 17:19:04 +0800

    another list style

--- live.py
+++ live.py
@@ -29,21 +29,31 @@ def sendmail(subject, content):
 		print 'email send failed.'
 
 livedict = {
-			'MAO' : 'maosh/1441569',
-			'YYT' : 'yuyintang_h/1217192',
-			'QSW' : '187956/11298220',
-			'OST' : '176416/10189365',
-			# 'HAL' : '273062/191469274',
-			'JZC' : 'jzclub/1357869',}
+			'MAO' : 'maosh/1441569/1', # https://site.douban.com/maosh/widget/events/1441569/
+			'YYT' : 'yuyintang_h/1217192/1',
+			'QSW' : '187956/11298220/1', # https://site.douban.com/187956/widget/events/11298220/
+			'OST' : '176416/10189365/1',
+			'JZC' : 'jzclub/1357869/1',
+			'HAL' : '273062/191469274/1', # https://site.douban.com/273062/widget/events/191469274/
+			'MSL' : '290170/192970720/2', # https://site.douban.com/290170/widget/events/192970720/
+			'696' : 'livebar696/1381481/1', # https://site.douban.com/livebar696/widget/events/1381481/
+			'YGS' : 'yugongyishan/1431074/2', # https://site.douban.com/yugongyishan/widget/events/1431074/
+			'MOG' : 'moguspace/191972683/1', # https://site.douban.com/moguspace/widget/events/191972683/
+			'DDC' : '237627/16619636/2' # https://site.douban.com/237627/widget/events/16619636/
+			}
 
 def fetchliveshow(livehouse):
 	baseurl = 'https://site.douban.com/' + livedict[livehouse].split('/')[0] + '/widget/events/' + livedict[livehouse].split('/')[1] + '/?start='
+	liststyle = int(livedict[livehouse].split('/')[2])
 	pagedepth = 10
 	pagecnt = 0
 	urlrequest = urllib2.Request(baseurl + str(pagecnt))
 	html_src = urllib2.urlopen(urlrequest).read()
 	parser = BeautifulSoup(html_src, "html.parser")
-	eventcnt = re.findall(r'\d+', parser.find('span', 'count').text)
+	try:
+		eventcnt = re.findall(r'\d+', parser.find('span', 'count').text)
+	except:
+		eventcnt = ['0']
 	if len(eventcnt):
 		pagecnt = int(eventcnt[0]) / pagedepth + 1
 	print pagecnt
@@ -52,9 +62,19 @@ def fetchliveshow(livehouse):
 		urlrequest = urllib2.Request(baseurl + str(i * pagedepth))
 		html_src = urllib2.urlopen(urlrequest).read()
 		parser = BeautifulSoup(html_src, "html.parser")
-		elist = parser.find('div', 'events-list-s').findAll('li', 'item ')
+		# liststyle 1: 'events-list-s', 'class':'item close' and 'class':'item '
+		# liststyle 2: 'events-list', 'class':'item'
+		if liststyle == 1:
+			elist = parser.find('div', {'class' : 'events-list-s'}).findAll('li', {'class' : 'item '})
+		elif liststyle == 2:
+			elist = parser.find('div', {'class' : 'events-list'}).findAll('li', {'class' : 'item'})
+		else:
+			elist = []
 		print len(elist), i
 		for event in elist:
+			if event.findNext('span').text.find(u'已结束') != -1:
+				elist = []
+				break
 			eventurl = event.findNext('a')['href']
 			urlrequest = urllib2.Request(eventurl)
 			html_src = urllib2.urlopen(urlrequest).read()
